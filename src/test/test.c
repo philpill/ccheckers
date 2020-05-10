b469e80957bf4a68b75b9104aefdb320f5a7067c
philpill <github@philpill.net>
2020-05-10 13:07:29 +0100

    tests

--- src/test/test.c
+++ src/test/test.c
@@ -11,27 +11,27 @@ int main(void)
 {
     const struct CMUnitTest tests[] =
     {
-        // cmocka_unit_test(test_get_pawn_moves_works),
-        // cmocka_unit_test(test_move_within_bounds),
-        // cmocka_unit_test(test_move_unoccupied_spaces),
-        // cmocka_unit_test(test_move_forward_0),
-        // cmocka_unit_test(test_move_forward_1_l),
-        // cmocka_unit_test(test_move_forward_1_r),
-        // cmocka_unit_test(test_move_forward_2),
-        // cmocka_unit_test(test_move_forward_3),
-        // cmocka_unit_test(test_calibrate_position),
-        // cmocka_unit_test(test_jump_move),
-        // cmocka_unit_test(test_invalid_jump_move),
-        // cmocka_unit_test(test_file_load),
-        // cmocka_unit_test(test_get_result_works),
-        // cmocka_unit_test(test_capture),
-        // cmocka_unit_test(test_board_orientation),
-        // cmocka_unit_test(test_promote_king),
-        // cmocka_unit_test(test_initial_population_0),
-        // cmocka_unit_test(test_get_jumps_works)
+        cmocka_unit_test(test_get_pawn_moves_works),
+        cmocka_unit_test(test_move_within_bounds),
+        cmocka_unit_test(test_move_unoccupied_spaces),
+        cmocka_unit_test(test_move_forward_0),
+        cmocka_unit_test(test_move_forward_1_l),
+        cmocka_unit_test(test_move_forward_1_r),
+        cmocka_unit_test(test_move_forward_2),
+        cmocka_unit_test(test_move_forward_3),
+        cmocka_unit_test(test_calibrate_position),
+        cmocka_unit_test(test_jump_move),
+        cmocka_unit_test(test_invalid_jump_move),
+        cmocka_unit_test(test_file_load),
+        cmocka_unit_test(test_get_result_works),
+        cmocka_unit_test(test_capture),
+        cmocka_unit_test(test_board_orientation),
+        cmocka_unit_test(test_promote_king),
+        cmocka_unit_test(test_initial_population_0),
+        cmocka_unit_test(test_get_jumps_works),
 
         cmocka_unit_test(test_get_options_length),
-         cmocka_unit_test(test_get_options_content),    
+        cmocka_unit_test(test_get_options_content),    
         cmocka_unit_test(test_get_set_option_mode)
 
     };
